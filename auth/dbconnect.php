aab1d064794721e2ab23425b85442057136206b0
et-Ghifari <et.ghifari@gmail.com>
2021-09-20 18:01:46 +0700

    update

--- auth/dbconnect.php
+++
@@ -1,7 +0,0 @@
-<?php
-$server     = 'localhost';
-$username   = 'root';
-$password   = '';
-$database   = 'pendaftaran';
-
-$conn       = mysqli_connect($server,$username,$password,$database) or die ('Koneksi gagal'.mysqli_connect_error());
