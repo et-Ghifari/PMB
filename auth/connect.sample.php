eaf4f1bf61c9dd7d65ac29a2a3b6b5f4ee52c544
et-Ghifari <et.ghifari@gmail.com>
2021-09-27 15:27:40 +0700

    update

--- auth/connect.sample.php
+++ auth/connect.sample.php
@@ -4,4 +4,4 @@ $dbusername = 'root';
 $dbpassword = '';
 $dbdatabase = 'pendaftaran';
 
-$conn     = mysqli_connect($dbserver, $dbusername, $dbpassword, $dbdatabase) or die('Koneksi gagal' . mysqli_connect_error());
+$conn       = mysqli_connect($dbserver, $dbusername, $dbpassword, $dbdatabase) or die('Koneksi gagal' . mysqli_connect_error());
--- auth/connectAuth.php
+++
@@ -1,7 +0,0 @@
-<?php
-$dbserver   = 'localhost';
-$dbusername = 'root';
-$dbpassword = '';
-$dbdatabase = 'pmb';
-
-$conn     = mysqli_connect($dbserver, $dbusername, $dbpassword, $dbdatabase) or die('Koneksi gagal' . mysqli_connect_error());
