a6cff5c633a94419afdd073dc971a0ef5e564f1c
et-Ghifari <et.ghifari@gmail.com>
2021-10-05 14:51:59 +0700

    update

---
+++ config/connect.php
@@ -0,0 +1,19 @@
+<?php
+date_default_timezone_set('Asia/Jakarta');
+session_start();
+
+$dbserver   = 'localhost';
+$dbusername = 'root';
+$dbpassword = '';
+$dbdatabase = 'pmb';
+
+$conn = mysqli_connect($dbserver, $dbusername, $dbpassword, $dbdatabase) or die('Koneksi gagal' . mysqli_connect_error());
+
+function base_url($url = null){
+    $base_url = 'http://localhost/pmb_dasar';
+    if($url != null){
+        return $base_url.'/'.$url;
+    }else{
+        return $base_url;
+    }
+}
